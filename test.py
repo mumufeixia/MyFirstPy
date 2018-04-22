b4ecc346cddc8255caf27916ec3a77aad8ed3d38
mumufeixia <liulin8796@126.com>
2018-04-22 16:07:06 +0800

    test 20180421

---
+++ test.py
@@ -0,0 +1,35 @@
+print "Hello \nWorld"
+
+
+marker = '@@%&'
+names = ['a' , 'b' , 'C']
+for name in names:
+    print marker + name
+
+size = 40
+a = b = c = 3
+a,b,c=3,4,5
+
+print 'hello w' \
+      'orld'
+print  '''1321
+342
+43214
+4312
+ret'''
+
+print len('hh')
+
+#list
+a = ['dgdg' , 'fg' ,100 ,1234 , 2*2]
+a[0] #'dgdg'
+a[:2]  # 'dgdg' , 'fg' ,
+a[2]=a[2]+23
+
+a , b = 0 ,1
+while(b < 1000):
+    print b,
+    a , b = b , a+b
+
+
+
