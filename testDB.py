7487a3199f84cab9d8a27675f225b34aa6d2f723
mumufeixia <liulin8796@126.com>
2018-04-24 21:51:06 +0800

    20180424 web bug

--- testDB.py
+++ testDB.py
@@ -2,6 +2,23 @@
 # -*- coding:utf8 -*-
 import MySQLdb
 
+'''
+DROP TABLE IF EXISTS EMPLOYEE
+CREATE TABLE EMPLOYEE (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,  
+         SEX CHAR(1),
+         INCOME FLOAT )        
+INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)         
+SELECT * FROM EMPLOYEE WHERE INCOME > 1000
+UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = M " 
+DELETE FROM EMPLOYEE WHERE AGE > 20
+
+'''
+
 # 打开数据库连接
 db = MySQLdb.connect("localhost", "root", "0", "Test", charset='utf8')
 
@@ -9,25 +26,28 @@ db = MySQLdb.connect("localhost", "root", "0", "Test", charset='utf8')
 cursor = db.cursor()
 
 # 如果数据表已经存在使用 execute() 方法删除表。
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+cursor.execute("show databases;")
 
-# 创建数据表SQL语句
-sql = """CREATE TABLE EMPLOYEE (
-         FIRST_NAME  CHAR(20) NOT NULL,
-         LAST_NAME  CHAR(20),
-         AGE INT,  
-         SEX CHAR(1),
-         INCOME FLOAT )"""
+# # 创建数据表SQL语句
+# sql = """CREATE TABLE EMPLOYEE (
+#          FIRST_NAME  CHAR(20) NOT NULL,
+#          LAST_NAME  CHAR(20),
+#          AGE INT,
+#          SEX CHAR(1),
+#          INCOME FLOAT )"""
+#
+# sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+#          LAST_NAME, AGE, SEX, INCOME)
+#          VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+#
+# sql = "SELECT * FROM EMPLOYEE \
+#        WHERE INCOME > '%d'" % (1000)
 
-sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
-         LAST_NAME, AGE, SEX, INCOME)
-         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+# 使用 fetchone() 方法获取一条数据
+data = cursor.fetchall()
 
-sql = "SELECT * FROM EMPLOYEE \
-       WHERE INCOME > '%d'" % (1000)
-
-
-cursor.execute(sql)
+print data
+# cursor.execute(sql)
 
 # 关闭数据库连接
 db.close()
