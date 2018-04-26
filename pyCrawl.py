6bd264ff17a53748ca2c47830e94a779b9e46784
mumufeixia <liulin8796@126.com>
2018-04-26 23:31:07 +0800

    python pyCrawl 20180426

--- pyCrawl.py
+++ pyCrawl.py
@@ -1,11 +1,32 @@
 # -*- coding:UTF-8 -*-
-# import sys
 import requests
-# reload(sys)
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-# sys.setdefaultencoding('uft-8')
-resp=requests.get('http://news.sina.com.cn/')
-resp.encoding = 'utf-8'
+driver = webdriver.PhantomJS('')
+url_link = "http://news.sina.com.cn/"
+driver.get(url_link)
+data = driver.title
+print data
+
+# import sys
+# import requests
+# reload(sys)
+# from bs4 import BeautifulSoup
+# # sys.setdefaultencoding('uft-8')
+# resp=requests.get('http://news.sina.com.cn/')
+# resp.encoding = 'utf-8'
+#
+# bs = BeautifulSoup(resp.text)
+#
+# links = bs.find_all('a')
+# for link in links:
+#     print link.get('href')
+# # for link in soup.find_all('a'):
+# #     print(link.get('href'))
+# #     # http://example.com/elsie
+
+
 htmlcontend =resp.text
 
 print type(htmlcontend)
--- test.py
+++ test.py
@@ -1,35 +1,61 @@
-print "Hello \nWorld"
+import os
+# for root,dirs,files in os.walk('.'):
+#     for file in files:
+#         file_path = os.path.join(root,file)
+#         # print file_path
+#         if '.html' in file_path:
+#             print file_path
 
-
-marker = '@@%&'
-names = ['a' , 'b' , 'C']
-for name in names:
-    print marker + name
-
-size = 40
-a = b = c = 3
-a,b,c=3,4,5
-
-print 'hello w' \
-      'orld'
-print  '''1321
-342
-43214
-4312
-ret'''
-
-print len('hh')
-
-#list
-a = ['dgdg' , 'fg' ,100 ,1234 , 2*2]
-a[0] #'dgdg'
-a[:2]  # 'dgdg' , 'fg' ,
-a[2]=a[2]+23
-
-a , b = 0 ,1
-while(b < 1000):
-    print b,
-    a , b = b , a+b
+# os.rename('./news.html','./news/news.html')
 
 
 
+
+
+# print "Hello \nWorld"
+#
+#
+# marker = '@@%&'
+# names = ['a' , 'b' , 'C']
+# for name in names:
+#     print marker + name
+#
+# size = 40
+# a = b = c = 3
+# a,b,c=3,4,5
+#
+# print 'hello w' \
+#       'orld'
+# print  '''1321
+# 342
+# 43214
+# 4312
+# ret'''
+#
+# print len('hh')
+#
+# #list
+# a = ['dgdg' , 'fg' ,100 ,1234 , 2*2]
+# a[0] #'dgdg'
+# a[:2]  # 'dgdg' , 'fg' ,
+# a[2]=a[2]+23
+#
+# a , b = 0 ,1
+# while(b < 1000):
+#     print b,
+#     a , b = b , a+b
+
+
+# with open('cal.py','r') as cal:
+#     cal_cont = cal.readline()
+#     print cal_cont
+#
+#
+# cal_file = open('cal.py','r')
+# cal_cont = cal_file.readlines()
+# print cal_cont[0]
+# cal_file.close()
+
+# cal_file = open('new.html','a')
+# cal_cont = cal_file.writelines(['1\n','2,3\n'])
+# cal_file.close()
